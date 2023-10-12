605fca37b865adec21678f2ccec15a2bac992498
jinsunee <jsunee.dev@gmail.com>
2023-10-12 17:13:10 +0900

    feat: [데이터 추가하기] 추가 버튼을 누르면 해당 날짜에 맞는 Todo Item을 추가할 수 있다. #5

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { FiCheck, FiChevronLeft, FiChevronRight, FiPlus } from "react-icons/fi";
-import "./App.css";
+import { v4 as uuidv4 } from "uuid";
 
 type TodoItem = {
   id: string;
@@ -52,7 +52,11 @@ function extractDateObject(date: Date) {
   const month = date.getMonth() + 1;
   const dateOfMonth = date.getDate();
 
-  return { year, month, date: dateOfMonth };
+  return { year, month, date: dateOfMonth } as DateObject;
+}
+
+function getDateString(dateObject: DateObject) {
+  return `${dateObject.year}-${dateObject.month}-${dateObject.date}`;
 }
 
 function shiftDateBy(date: DateObject, diff: number) {
@@ -65,16 +69,29 @@ function App() {
   const [currentDate, setCurrentDate] = useState<DateObject>(
     extractDateObject(new Date())
   );
-  const [todoItems, setTodoItems] = useState<TodoItem[]>();
+  const [todoItems, setTodoItems] = useState<TodoItem[]>([]);
+  const [mode, setMode] = useState<{
+    type: "add" | "edit" | "default";
+    id?: string;
+  }>({ type: "default" });
+  const [addInput, setAddInput] = useState<string>("");
+  const [editInput, setEditInput] = useState<string>("");
 
   useEffect(() => {
     setTodoItems(defaultItems);
-  }, []);
+  }, [currentDate]);
 
   function handleDone(index: number) {
     const newTodoItems = [...todoItems!];
     newTodoItems[index].isDone = !newTodoItems[index].isDone;
     setTodoItems(newTodoItems);
+  }
+
+  function handleCancelAddInput() {
+    setMode({
+      type: "default",
+    });
+    setAddInput("");
   }
 
   return (
@@ -121,6 +138,80 @@ function App() {
           gap: 15,
         }}
       >
+        {mode.type === "add" && (
+          <div>
+            <input
+              type="text"
+              value={addInput}
+              onChange={(e) => setAddInput(e.target.value)}
+              placeholder="이곳에 할일을 추가할 수 있어요  :)"
+              style={{
+                width: "100%",
+                background: "none",
+                border: "none",
+                borderBottom: "1px solid #666",
+                padding: "7px 0",
+                fontWeight: "bold",
+                fontSize: 16,
+                color: "#fff",
+              }}
+            />
+            <div style={{ height: 5 }} />
+            <div style={{ display: "flex", gap: 5, alignItems: "center" }}>
+              <button
+                style={{
+                  background: "none",
+                  border: "1px solid #CFFF48",
+                  borderRadius: 10,
+                  padding: "5px 7px",
+                  color: "#CFFF48",
+                  fontWeight: "bold",
+                }}
+                onClick={handleCancelAddInput}
+              >
+                취소
+              </button>
+              <button
+                style={{
+                  background: "#CFFF48",
+                  border: "none",
+                  borderRadius: 10,
+                  padding: "5px 7px",
+                  color: "#1E1E1E",
+                  fontWeight: "bold",
+                }}
+                onClick={() => {
+                  const lastIndex = localStorage.getItem("lastIndex") ?? "0";
+                  const newLastIndex = parseInt(lastIndex) + 1;
+
+                  const newTodoItem = {
+                    id: uuidv4(),
+                    title: addInput,
+                    isDone: false,
+                    index: newLastIndex,
+                    createdAt: getDateString(currentDate),
+                  };
+
+                  const originalTodoItems = JSON.parse(
+                    localStorage.getItem("todoItems") ?? "[]"
+                  );
+
+                  localStorage.setItem("lastIndex", newLastIndex.toString());
+                  localStorage.setItem(
+                    "todoItems",
+                    JSON.stringify([...originalTodoItems, newTodoItem])
+                  );
+
+                  setTodoItems([...todoItems, newTodoItem]);
+
+                  handleCancelAddInput();
+                }}
+              >
+                저장
+              </button>
+            </div>
+          </div>
+        )}
         {todoItems?.map((item, index) => (
           <div
             key={item.id}
@@ -166,6 +257,7 @@ function App() {
           right: 20,
           bottom: 20,
         }}
+        onClick={() => setMode({ type: "add" })}
       >
         <FiPlus color="#1E1E1E" size={23} />
       </div>
